56a0defc61c787caa6067af082de6d8956e323cc
samardp <samarendra.digitalpiloto@gmail.com>
2024-10-14 17:57:18 +0530

    Update nltk.py

--- nltk.py
+++ nltk.py
@@ -1,12 +1,12 @@
 import nltk
 import os
 
-# Define the NLTK data directory
-nltk_data_dir = '/nltk_data'  # Change this to the path where nltk_data should be stored
+# Set up a path within the home directory for nltk data
+home_dir = os.path.expanduser("~")
+nltk_data_dir = os.path.join(home_dir, 'nltk_data')
 if not os.path.exists(nltk_data_dir):
     os.makedirs(nltk_data_dir)
 nltk.data.path.append(nltk_data_dir)
 
 # Ensure punkt is downloaded
 nltk.download('punkt', download_dir=nltk_data_dir)
-
