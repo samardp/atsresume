98fcdc6af152e7407869d46689993da6a13d831d
samardp <samarendra.digitalpiloto@gmail.com>
2024-10-14 17:47:42 +0530

    Update nltk.py

--- nltk.py
+++ nltk.py
@@ -2,7 +2,7 @@ import nltk
 import os
 
 # Define the NLTK data directory
-nltk_data_dir = '/path/to/nltk_data'  # Change this to the path where nltk_data should be stored
+nltk_data_dir = '/nltk_data'  # Change this to the path where nltk_data should be stored
 if not os.path.exists(nltk_data_dir):
     os.makedirs(nltk_data_dir)
 nltk.data.path.append(nltk_data_dir)
