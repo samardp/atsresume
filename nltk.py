c51025b30366f687f1100202d718bd12581b9006
samardp <samarendra.digitalpiloto@gmail.com>
2024-10-14 17:44:03 +0530

    Update nltk.py

--- nltk.py
+++ nltk.py
@@ -1,1 +1,12 @@
+import nltk
+import os
+
+# Define the NLTK data directory
+nltk_data_dir = '/path/to/nltk_data'  # Change this to the path where nltk_data should be stored
+if not os.path.exists(nltk_data_dir):
+    os.makedirs(nltk_data_dir)
+nltk.data.path.append(nltk_data_dir)
+
+# Ensure punkt is downloaded
+nltk.download('punkt', download_dir=nltk_data_dir)
 
